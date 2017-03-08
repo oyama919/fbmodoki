5b60698e77a2e77cf37ca09063e8c4c3d62ff50e
oyama919 <oyama919@gmail.com>
2017-03-08 23:00:06 +0900

    Topics CRUD 作成

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -1,16 +1,54 @@
 class TopicsController < ApplicationController
+  before_action :set_topic, only: [:show, :edit, :update, :destroy]
+
   def index
+    @topics = Topic.all
+  end
+
+  def new
+    if params[:back]
+      @topic = Topic.new(topics_params)
+    else
+      @topic = Topic.new
+    end
+  end
+
+  def confirm
+    @topic = Topic.new(topics_params)
   end
 
   def create
-  end
-
-  def show
+     @topic = Topic.new(topics_params)
+     if @topic.save
+       redirect_to topics_path(@topic)
+     else
+       render 'new'
+     end
   end
 
   def edit
   end
 
-  def delete
+  def update
+    if @topic.update(topics_params)
+      redirect_to topics_path
+    else
+      render action: 'edit'
+    end
   end
+
+  def destroy
+    @topic.destroy
+  end
+
+  def show
+  end
+
+  private
+    def topics_params
+      params.require(:topic).permit(:title,:content)
+    end
+    def set_topic
+      @topic = Topic.find(params[:id])
+    end
 end
