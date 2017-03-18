7bc3a1b584321f142410162851a701487dfba442
oyama919 <oyama919@gmail.com>
2017-03-18 13:20:03 +0900

    gem faker テスト用　シードデータを追加できるように

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,21 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+100.times do |n|
+  email = Faker::Internet.email
+  password = "password"
+  User.create!(email: email,
+               password: password,
+               password_confirmation: password,
+               )
+end
+
+n = 1
+while n <= 100
+  Topic.create(
+    title: "test",
+    content: "Lorem sed do eiusmod tempor labore et dolore magna aliqua. Ut enim ad minim veniam, laboris nisi ut aliquip ex ",
+    user_id: n
+  )
+  n = n + 1
+end
