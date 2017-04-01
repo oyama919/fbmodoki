7a00ba0cb475fdadbfb2f0328fe9e77bd2470e9e
oyama919 <oyama919@gmail.com>
2017-04-01 22:07:39 +0900

    fix

--- config/deploy.rb
+++ config/deploy.rb
@@ -7,7 +7,7 @@ set :application, 'fbmodoki'
 set :repo_url, 'https://github.com/oyama919/fbmodoki'
 
 # deployするブランチ。デフォルトはmasterなのでなくても可。
-set :branch, ENV['BRANCH'] || 'fb'
+set :branch, ENV['BRANCH'] || 'master'
 
 # deploy先のディレクトリ。
 set :deploy_to, '/var/www/fbmodoki'
